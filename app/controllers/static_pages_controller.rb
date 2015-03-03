4629b1086894d5ce3de605d8827c27640f8273d8
succi0303 <succi0303@gmail.com>
2015-03-03 13:33:14 +0900

    top page enhancement

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def index
     @tags = ActsAsTaggableOn::Tag.most_used
+    @recent_binders = Binder.order('updated_at DESC').limit(10)
+    @popular_binders = Binder.order('view_count DESC, updated_at DESC').limit(10)
   end
 end
